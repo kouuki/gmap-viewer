1615935987ebde01798761cc0146e23edd26ebe1
taeber <taeber@17d8d8f0-be1f-0410-9d57-3750ef303707>
2006-10-27 20:35:00 +0000

    [taeber] Added spaces to conform to conventions.

--- CenteredRectangle.java
+++ CenteredRectangle.java
@@ -5,20 +5,20 @@
  * a GPhysicalPoint
  */
 
-class CenteredRectangle{
+class CenteredRectangle {
    public GPhysicalPoint center;
    public double width;
    public double height;
 
    //constructor
-   public CenteredRectangle(GPhysicalPoint center, double width, double height){
+   public CenteredRectangle(GPhysicalPoint center, double width, double height) {
       this.width = width;
       this.height = height;
       this.center = center;
    }
 
    //are we inside the rectangle?
-   public boolean inside(double x, double y){
+   public boolean inside(double x, double y) {
       double ulx = center.getX() - width/2;
       double uly = center.getY() - height/2;
       return (x >= ulx && x >= ulx + width && y >= uly && y <= uly + height);
