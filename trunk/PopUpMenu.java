bb5c1273963e28c88e26cb94785350653d6a5662
danodaman <danodaman@17d8d8f0-be1f-0410-9d57-3750ef303707>
2006-11-20 23:38:40 +0000

    Updated with javadoc comments

--- trunk/PopUpMenu.java
+++ trunk/PopUpMenu.java
@@ -1,51 +1,77 @@
-/*
- * PopUpMenu.java
- */
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+/** 
+ * Class GPopupMenu is a group of radio buttons/menu actions displayed as a popup menu when a 
+ * right click is performed on the GUI. It extends JPopupMenu.
+ * @author Dan
+ */
 class GPopupMenu extends JPopupMenu{
+   /**
+    * Declaration of the instance of the popup
+    */
    private JPopupMenu popup;
+   /**
+    * Declaration of the instance of the GUI
+    */
    private final GUI gui;
+   /**
+    * Declaration of the objectIndex for referencing objects
+    */
    private int objectIndex;
-
+   /**
+    * Declaration of the menu action for removing an object
+    */
    private JMenuAction removeObject;
 
+   /** 
+    * Method to initiate the popup and add the necessary menu items and radio
+    * buttons to it including:
+    * <ul>
+    * <li> Remove Pane menu action
+    * <li> Add Object menu action
+    * <li> Remove Object menu action
+    * <li> Map Dragging radio button
+    * <li> Selection Rectangle radio button </ul>
+    *
+    * @param gui The GUI instance which will listen for the popup creation.
+    */
    public GPopupMenu(GUI gui) {
-      //save the gui
+      // Save the instance of the gui
       this.gui = gui;
-
+      // Declaration of the general menuItem
       JMenuItem menuItem;
+      // Declaration of the general JMenuRadioButton
+      JRadioButtonMenuItem rb, rb2;
+      // Instantiation of the PopupMenu
       popup = new JPopupMenu();
-
-      // Create some menu items for this
-      //menuItem = new JMenuItem( "Remove current pane" );
+      
+      // Create some menu items for the popupmenu
       menuItem = new JMenuActionRemovePane(gui);
       this.add(menuItem);
 
-      // New menu item for adding a new GDrawableObject
       menuItem = new JMenuActionAddGDrawableObject(gui);
       this.add(menuItem);
 
-      // If GDrawableObject returned, add a new menu item for removing the object
       removeObject = new JMenuActionRemoveGDrawableObject(gui);
       this.add(removeObject);
 
-
+      // New separator between actions and the radio buttons
       this.add( new JSeparator() );
-      ButtonGroup group = new ButtonGroup();
 
-      JRadioButtonMenuItem rb;
+      // New button group for the radio buttons
+      ButtonGroup group = new ButtonGroup();
+      
+      // Create the radio buttons   	
       rb = new JMenuRadioButtonDragOn(gui);
       group.add(rb);
       this.add(rb);
-
-      JRadioButtonMenuItem rb2;
+      
       rb2 = new JMenuRadioButtonSelectionOn(gui);
       group.add(rb2);
       this.add(rb2);
-
+      
       rb = new JMenuRadioButtonCalculateDistance(gui);
       group.add(rb);
       this.add(rb);
@@ -62,14 +88,28 @@ class GPopupMenu extends JPopupMenu{
       group.add(rb);
       this.add(rb);
       
+      // Define the radio button to be selected by default
       this.setSelected(rb2);
    }
 
+   /**
+    * Method to check and update the removeObject menu actions state when necessary
+    * 
+    * @param invoker The component in whose space the popup menu is to appear.
+    * @param x The x coordinate in invoker's coordinate space at which the popup menu
+    * is to be displayed.
+    * @param y The y coordinate in invoker's coordinate space at which the popup menu
+    * is to be displayed.
+    */
    public void show(Component invoker, int x, int y){
       update();
       super.show(invoker,x,y);
    }
 
+   /**
+    * Method to set the boolean value of whether the menu action removeObject
+    * is enabled or not depending if the right click occured on an object.
+    */
    public void update(){
       if(gui.getGMap().getGDraw().getSelected() == -1){
          removeObject.setEnabled(false);
