a3068c742e60128b0d7d9f79f37f31d6c300b358
danodaman <danodaman@17d8d8f0-be1f-0410-9d57-3750ef303707>
2006-11-21 03:26:49 +0000

    Added javadoc comments

--- EmbeddedProgressMeter.java
+++ EmbeddedProgressMeter.java
@@ -1,8 +1,3 @@
-/*
- * EmbeddedProgressMeter.java
- *
- */
-
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -18,17 +13,26 @@ import java.net.*;
 import javax.imageio.ImageIO;
 import java.awt.geom.*;
 
+/**
+ * Class EmbeddedProgressMeter operates as a controller class for ProgressMeter. It
+ * performs updates for the current position and status of the bar, painting these changes
+ * to the panel when necessary. It extends ProgressMeter and implements MouseListener.
+ */
 public class EmbeddedProgressMeter extends ProgressMeter implements MouseListener{
-
+   /** Declaration of the panel instance for the progress bar */
    private JPanel panel;
+   /** Declaration of the actual progress bar instance */
    private JProgressBar progressBar;
+   /** Declaration of a button for canceling any action using the progress bar */
    private JButton cancel;
+   /** Declaration of message displaying the status of the progress bar */
    private JLabel messageLabel;
+   /** Declaration of a thread instance used by the progress bar */
    private Thread thread;
-
+   /** Declaration of default string message for when the progress bar is not in use */
    private static final String notWorkingMessage = "Done.";
 
-    /** Creates a new instance of EmbeddedProgressMeter */
+   /** Creates a new instance of EmbeddedProgressMeter */
    public EmbeddedProgressMeter() {
       super(null,0.0,notWorkingMessage);
 
@@ -55,6 +59,13 @@ public class EmbeddedProgressMeter extends ProgressMeter implements MouseListene
       update();
    }
 
+   /** 
+    * Method to set the message for the current status of the progress bar
+    *
+    * @param message The message to display next to the progress bar
+    * @param o       The object of the progress bar
+    * @return        The boolean result for whether the message was set or not 
+    */
    public boolean setMessage( String message, Object o ) {
       if( super.setMessage( message, o ) == true ) {
          update();
@@ -64,7 +75,14 @@ public class EmbeddedProgressMeter extends ProgressMeter implements MouseListene
       return false;
       }
    }
-
+   
+   /** 
+    * Method to set the percentage for the progress bar
+    *
+    * @param percent The percentage to be set for the progress bar 
+    * @param o       The object of the progress bar
+    * @return        The boolean result for whether the percent was set or not 
+    */
    public boolean setPercent( double percent, Object o ) {
       if ( super.setPercent( percent, o ) == true ) {
          update();
@@ -74,7 +92,8 @@ public class EmbeddedProgressMeter extends ProgressMeter implements MouseListene
          return false;
       }
    }
-
+   
+   /** Method to update the information for the progress bar to the screen */
    private void update() {
       //further methods called here - not sure how the controller was to handle this
       progressBar.setValue((int)getRoundedPercent(1));
@@ -83,10 +102,20 @@ public class EmbeddedProgressMeter extends ProgressMeter implements MouseListene
       progressBar.paintImmediately( 0, 0, progressBar.getWidth(), progressBar.getHeight() );
    }
 
+   /** 
+    * Method to get the panel instance of the progress bar 
+    *
+    * @return The panel instance of the progress bar
+    */
    public JPanel getPanel() {
       return panel;
    }
 
+   /** 
+    * Method to check whether an object instance was grabbed by the progress bar
+    * 
+    * @return The boolean result for whether an object instance was grabbed by the progress bar
+    */
    public boolean grab( Object o ) {
       if( super.grab( o ) == true ) {
          progressBar.setStringPainted(true);
@@ -99,6 +128,11 @@ public class EmbeddedProgressMeter extends ProgressMeter implements MouseListene
       }
    }
 
+   /** 
+    * Method to check whether an object instance was released by the progress bar
+    * 
+    * @return The boolean result for whether an object instance was released by the progress bar
+    */
    public boolean release( Object o ) {
       if( super.release( o ) == true ) {
          progressBar.setStringPainted(false);
@@ -114,6 +148,11 @@ public class EmbeddedProgressMeter extends ProgressMeter implements MouseListene
       }
    }
 
+   /** 
+    * Method to create a new thread for a progress bar instance
+    * 
+    * @return The boolean result for whether a new thread was created
+    */
    public boolean registerThread(Thread thread, Object o){
       if(super.isObject(o)) {
          this.thread = thread;
@@ -124,12 +163,15 @@ public class EmbeddedProgressMeter extends ProgressMeter implements MouseListene
       }
    }
 
+   /** 
+    * Method to immediately stop the current thread action used by the progress bar
+    */
    public void stop(){
       super.stop();
       if(thread != null) thread.interrupt();
    }
 
-   //mouse methods - use e.getX()
+   // Mouse methods - use e.getX()
    public void mouseMoved(MouseEvent e) {}
    public void mouseDragged(MouseEvent e){}
    public void mouseClicked(MouseEvent e){
