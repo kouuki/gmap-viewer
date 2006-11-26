66fd18fef842973cd22c355dd8edb1d0f6cbe17e
danodaman <danodaman@17d8d8f0-be1f-0410-9d57-3750ef303707>
2006-11-26 23:37:49 +0000

    Added javadoc comments as well as a start to functionality for setting the cache directory.

--- CenteredRectangle.java
+++ CenteredRectangle.java
@@ -1,23 +1,31 @@
-/*
- * CenteredRectangle
- *
- * This class is a rectangle of size w,h centered on
+/**
+ * Class CenteredRectangle represents a rectangle of size w,h centered on
  * a GPhysicalPoint.
  */
-
 class CenteredRectangle {
+   /** Declaration of the center position of the rectangular shape */
    public GPhysicalPoint center;
+   /** Declaration of the width of the rectangle */
    public double width;
+   /** Declaration of the height of the rectangle */
    public double height;
 
-   //constructor
+   /** 
+    * Method to create a new instance of the rectangle
+    */
    public CenteredRectangle(GPhysicalPoint center, double width, double height) {
       this.width = width;
       this.height = height;
       this.center = center;
    }
 
-   //are we inside the rectangle?
+   /** 
+    * Method to check whether a particular (x,y) location is inside the rectangle
+    *  
+    * @param x The x coordinate to be checked
+    * @param y The y coordinate to be checked
+    * @return  The boolean result of whether the corresponding point (x,y) is in the rectangle
+    */
    public boolean inside(double x, double y) {
       double ulx = center.getX() - width/2;
       double uly = center.getY() - height/2;
