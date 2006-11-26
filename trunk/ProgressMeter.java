3bdb03ab2c86b85924c8346f7062dc24e7c2fc59
danodaman <danodaman@17d8d8f0-be1f-0410-9d57-3750ef303707>
2006-11-26 23:37:49 +0000

    Added javadoc comments as well as a start to functionality for setting the cache directory.

--- trunk/ProgressMeter.java
+++ trunk/ProgressMeter.java
@@ -2,8 +2,10 @@ import java.lang.String;
 import java.lang.Object;
 import java.lang.Math;
 
-
-
+/**
+ * Class ProgressMeter represents the characteristics of the progress meter 
+ * which are modified by EmbeddedProgressMeter
+ */
 class ProgressMeter{
 
    private Object object;
@@ -11,6 +13,7 @@ class ProgressMeter{
    private String message;
    private boolean stopFlag;
 
+   /** Creates a new instance of ProgressMeter */
    public ProgressMeter(String object, double percent, String message){
       this.object = object;
       this.percent = percent;
@@ -18,11 +21,18 @@ class ProgressMeter{
       this.stopFlag = false;
    }
 
+   /** Creates a new empty instance of ProgressMeter */
    public ProgressMeter(){
       this(null,0.0,"");
    }
 
-
+   /** 
+    * Method to set the message for the current status of the progress bar
+    *
+    * @param message The message to display next to the progress bar
+    * @param o       The object of the progress bar
+    * @return        The boolean result for whether the message was set or not 
+    */
    public boolean setMessage(String message, Object o){
       if(object == o){
          this.message=message;
@@ -33,6 +43,13 @@ class ProgressMeter{
       }
    }
 
+   /** 
+    * Method to set the percentage for the progress bar
+    *
+    * @param percent The percentage to be set for the progress bar 
+    * @param o       The object of the progress bar
+    * @return        The boolean result for whether the percent was set or not 
+    */
    public boolean setPercent(double percent, Object o){
       if(object == o){
          this.percent=percent;
@@ -43,14 +60,29 @@ class ProgressMeter{
       }
    }
 
+   /**
+    * Method to get the current percent value of the progress bar
+    *
+    * @return The current percent value of the progress bar
+    */
    public double getPercent(){
       return percent;
    }
 
+   /**
+    * Method to get the status message of the progress bar
+    *
+    * @return The current status message of the progress bar
+    */
    public String getMessage(){
       return message;
    }
 
+   /** 
+    * Method to check whether an object instance was grabbed by the progress bar
+    * 
+    * @return The boolean result for whether an object instance was grabbed by the progress bar
+    */
    public boolean grab(Object o){
       if(object == null){
          object=o;
@@ -62,6 +94,11 @@ class ProgressMeter{
       }
    }
 
+   /** 
+    * Method to check whether an object instance was released by the progress bar
+    * 
+    * @return The boolean result for whether an object instance was released by the progress bar
+    */
    public boolean release(Object o){
       if(object== o || object == null){
          object=null;
@@ -72,24 +109,48 @@ class ProgressMeter{
          return false;
       }
    }
-
+   
+   /** 
+    * Method to immediately stop the current thread action used by the progress bar
+    */
    public void stop(){
       stopFlag = true;
    }
 
+   /** 
+    * Method to get the flag value of stopFlag
+    *
+    * @return The boolean result of stopFlag
+    */
    public boolean getStopFlag(){
       return stopFlag;
    }
 
-
+   /** 
+    * Method to check whether the passed paramater o is an object
+    *
+    * @param o The object passed for checking
+    * @return  The boolean result of whether the paramater is indeed an object
+    */
    public boolean isObject(Object o){
       return o == object;
    }
-
+   
+   /** 
+    * Method for making the progress bar visible or not
+    * 
+    * @param b The boolean value for setting the progress bar visible or not  
+    */
    public void setVisible(boolean b){
-      /*This function was said to do nothing.  Guess the child will put something here later*/
+      // Simple declared method for making the progress bar visible or not
    }
 
+   /** 
+    * Method for converting the job's integer total into a usable percentage
+    * 
+    * @param p  The integer value to be converted into a usable percentage value
+    * @return   The double usable percentage value
+    */
    public double getRoundedPercent(int p){
       long factor = (long)Math.pow(10,p);
       double val = percent;
@@ -104,6 +165,14 @@ class ProgressMeter{
       // back to the left.
       return (double)tmp / factor;
    }
+   
+   /** 
+    * Method for converting the job's complete status into a percentage
+    * 
+    * @param done    The double value of how much is already complete for the current job
+    * @param total   The double value of the job's total value needed for completion
+    * @return        The percentage value of how complete the job is
+    */
    public static double computePercent(double done, double total){
       return (done/total)*100.0;
    }
